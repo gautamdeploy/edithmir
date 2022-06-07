25c921b66db00fc95ca307a949b5ad813be26991
Heroqgit <106860620+Heroqgit@users.noreply.github.com>
2022-06-07 21:04:19 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -123,6 +123,11 @@ def get_readable_message():
                 globals()['PAGE_NO'] -= 1
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
             msg += f"<b>Name:</b> <code>{escape(str(download.name()))}</code>"
+            if update.message.from_user.username:
+                tag = f"@{update.message.from_user.username}"
+            else:
+                tag = update.message.from_user.mention_html(update.message.from_user.first_name)
+            msg += f"<b>cc:</b> <code>{tag}</code>"
             msg += f"\n<b>Status:</b> <i>{download.status()}</i>"
             if download.status() not in [
                 MirrorStatus.STATUS_ARCHIVING,
